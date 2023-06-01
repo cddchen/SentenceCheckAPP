5c927bffab81dfa08a5d501eb6df401d8accb919
cddchen <1354903898@qq.com>
2023-06-01 17:15:05 +0800

    Initial Commit

---
+++ Sentence/SentenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentenceApp.swift
+//  Sentence
+//
+//  Created by cdd on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SentenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
